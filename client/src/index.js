59a57b136be442e6f16b2c14d5a9f866dd3b4d27
yankechao <yankechao@vipkid.com.cn>
2018-09-13 22:29:03 +0800

    init code

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware} from 'redux'
+import rootReducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reduxLogger from 'redux-logger'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, reduxLogger)))
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
