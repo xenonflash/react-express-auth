ed16103f1d338ad4afd7162a6aebc29cb97e793a
yankechao <yankechao@vipkid.com.cn>
2018-09-14 00:04:44 +0800

    install antd

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'antd'
+
 class App extends Component {
   render() {
     return (
       <div className={'App'}>
         home
+        <Button>click</Button>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.min.css'
 import { createStore, applyMiddleware} from 'redux'
 import rootReducer from './reducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
