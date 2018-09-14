3c9f2153c22e78232f21244e3958621358b3e4d9
yankechao <yankechao@vipkid.com.cn>
2018-09-14 20:51:19 +0800

    simulate /login controller

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,7 +1,26 @@
+import mongoose from 'mongoose'
+
 const auth = {}
 
 auth.login = function(req, res, next){
-  res.json({msg: 'login'})
+  const username = req.bofy.username
+  const password = req.bofy.password
+  if(!username || !password){
+    res.json({
+      code: 400,
+      msg: 'param invalid'
+    })
+  } else if (username == 'admin' && password == '11235') {
+    res.json({
+      code: 200,
+      data: { username: 'admin', id: 1, avatar: null, email: 'a@b.com' }
+    })
+  } else {
+    res.json({
+      code: 401,
+      msg: 'username or password invalid'
+    })
+  }
 }
 auth.register = function(req, res, next){
   res.send({ msg: 'register' })
