27b825141b68df7821a14c36d64d1d844ca08d4d
yankechao <yankechao@vipkid.com.cn>
2018-09-14 19:52:23 +0800

    添加 user action 和 reducer

--- client/src/actions/auth.action.js
+++ client/src/actions/auth.action.js
@@ -5,9 +5,10 @@ export function login(formValues) {
   return dispatch => {
     dispatch({type: LOGIN_REQUEST})
     authApi.login(formValues).then(res => {
-      dispatch({type: LOGIN_SUCCESS, user: res})
+      dispatch({type: LOGIN_SUCCESS})
+      dispatch({type: SET_USER, user: res})
     }).catch(err => {
-      dispatch({type: LOGIN_FAIL, user: null})
+      dispatch({type: LOGIN_FAIL})
     })
   }
 }
@@ -15,9 +16,9 @@ export function register(formValues) {
   return dispatch => {
     dispatch({type: REGISTER_REQUEST})
     authApi.register(formValues).then(res => {
-      dispatch({type: REGISTER_SUCCESS, user: res})
+      dispatch({type: REGISTER_SUCCESS})
     }).catch(err => {
-      dispatch({type: REGISTER_FAIL, user: null})
+      dispatch({type: REGISTER_FAIL})
     })
   }
 }
@@ -28,3 +29,4 @@ export const LOGIN_FAIL = Symbol()
 export const REGISTER_REQUEST = Symbol()
 export const REGISTER_SUCCESS = Symbol()
 export const REGISTER_FAIL = Symbol()
+export const SET_USER = Symbol()
